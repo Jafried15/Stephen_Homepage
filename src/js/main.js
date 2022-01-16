33c6f2974cb3f9a1ebe0d4d4976e767f7a4941e1
Joél Friedrich <joel.friedrich@sersoft.de>
2022-01-16 08:10:55 +0100

    Finish Start Slider and Layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
-jQuery(function () {
-});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    this.showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    this.showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName('slide');
+    const dots = document.getElementsByClassName('dot');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex - 1].classList.add('active');
+    dots[slideIndex - 1].classList.add('active');
+}
